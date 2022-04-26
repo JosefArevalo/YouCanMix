8fcd9133661b8bbdc9eb9b017caadba25fae9f5f
Michael Flaherty <michaelwflaherty@me.com>
2022-04-25 19:55:35 -0700

    relocate resources & change Drink constructor

--- src/main/java/Drink/Drink.java
+++ src/main/java/Drink/Drink.java
@@ -1,30 +1,16 @@
 package Drink;
 
 public class Drink {
-    private String drinkName;
-    private String ingredients;
-    private String quantities;
+    private final String drinkName;
+    private final String ingredients;
+    private final String quantities;
     private int rating;
-    
-    //DEFAULT CONSTRUCTOR
-    public Drink() {
-    	super();
-    	this.drinkName = "";
-        this.ingredients = "";
-        this.quantities = "";
-        this.rating = 0;
-    }
 
-    //CONSTRUCTOR
     public Drink(String drinkName, String ingredients, String quantities, int rate) {
-        if (drinkName != null && ingredients != null && quantities != null)
-        {
-            this.drinkName = drinkName;
-            this.ingredients = ingredients;
-            this.quantities = quantities;
-            this.rating = rate;
-        }
-        
+        this.drinkName = drinkName;
+        this.ingredients = ingredients;
+        this.quantities = quantities;
+        this.rating = rate;
     }
     
     public String getDrinkName() {
